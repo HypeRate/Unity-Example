8f356cf1839a89b8e000924d4f8bbc67ddebecbd
Wofsauge <jan-.-@t-online.de>
2022-02-06 00:43:31 +0100

    Added hit score display

---
+++ Assets/ScoreTextHandler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTextHandler : MonoBehaviour
+{
+    GameObject grandChild;
+    public float moveAmount = 10;
+    public Color fadeColor;
+    public float fadeTime = 4.0f;
+    public string displayText = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        grandChild = this.gameObject.transform.GetChild(0).GetChild(0).gameObject;
+        grandChild.GetComponent<Text>().text = displayText;
+        Object.Destroy(this.gameObject, fadeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grandChild.transform.position += new Vector3(0, moveAmount * Time.deltaTime, 0);
+        Color objColor = grandChild.GetComponent<Text>().color;
+        grandChild.GetComponent<Text>().color = Color.Lerp(objColor, fadeColor, fadeTime * Time.deltaTime);
+    }
+}
--- Assets/shooting.cs
+++ Assets/shooting.cs
@@ -8,12 +8,14 @@ public class shooting : MonoBehaviour
     private InputAction click;
 
     public GameObject entryHole;
+    public GameObject scoreDisplayObject;
     public float surfaceOffset = 0.1f;
 
     void Awake()
     {
         click = new InputAction(binding: "<Mouse>/leftButton");
-        click.performed += ctx => {
+        click.performed += ctx =>
+        {
             RaycastHit hit;
             float x = Screen.width / 2f;
             float y = Screen.height / 2f;
@@ -21,12 +23,16 @@ public class shooting : MonoBehaviour
             var ray = Camera.main.ScreenPointToRay(new Vector3(x, y, 0));
             if (Physics.Raycast(ray, out hit))
             {
+                float scoreAcc = 0;
                 if (hit.collider.tag == "Score Target")
                 {
                     float dist = Vector3.Distance(hit.collider.transform.position, hit.point);
-                    Debug.Log("Hit cool!"+ dist);
+                    scoreAcc = 10 - Mathf.Min(10, Mathf.Floor(dist / 0.045f));
                 }
-                Instantiate(entryHole, hit.point+ hit.normal* surfaceOffset, Quaternion.FromToRotation(new Vector3(0,1,0), hit.normal));
+                Instantiate(entryHole, hit.point + hit.normal * surfaceOffset, Quaternion.FromToRotation(new Vector3(0, 1, 0), hit.normal));
+
+                var scoreObj = Instantiate(scoreDisplayObject, new Vector3(0, 0, 0), Quaternion.identity);
+                scoreObj.GetComponent<ScoreTextHandler>().displayText = scoreAcc + " Points!";
             }
         };
         click.Enable();
